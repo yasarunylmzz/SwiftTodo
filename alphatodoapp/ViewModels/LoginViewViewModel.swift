87870be8a2c0d7f94eee139a03fe5672d90006f1
Yaşar Ünyılmaz <116540999+yasarunylmzz@users.noreply.github.com>
2024-05-26 14:11:45 +0300

    change validation

--- alphatodoapp/ViewModels/LoginViewViewModel.swift
+++ alphatodoapp/ViewModels/LoginViewViewModel.swift
@@ -19,20 +19,25 @@ class LoginViewViewModel: ObservableObject{
     }
     
     func login(){
+        guard validate() else{
+            return 
+        }
+    }
+    
+    func validate() -> Bool{
         errorMessages = ""
         // Burada pass ve email boş olunca mesaj döndürüyor
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
         !password.trimmingCharacters(in: .whitespaces).isEmpty
         else{
             errorMessages = "Lütfen Tüm Alanları Doldurunuz"
-            return
+            return false
         }
         //burada ise email kontrolü yapıyor.
         guard !email.contains("@") && email.contains(".") else{
             errorMessages = "Lütfen Geçerli bir Email Adresi Giriniz"
-            return
+            return false
         }
+        return true
     }
-    
-    func validate(){}
 }
