079f7e8bf6128afed56a90cef29026e127cce1e4
Yaşar Ünyılmaz <116540999+yasarunylmzz@users.noreply.github.com>
2024-05-26 12:01:30 +0300

    Initial Commit

---
+++ alphatodoapp/alphatodoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  alphatodoappApp.swift
+//  alphatodoapp
+//
+//  Created by Yaşar Ünyılmaz on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct alphatodoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
